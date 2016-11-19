ee7016cdd42f22f3972b539abd22efd659a75d3c
juampiii <jp.rodriguez.valentin@gmail.com>
2016-11-19 17:23:06 +0100

    simple home with bootstrap+blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('newone')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Home</div>
+
+                    <div class="panel-body">
+                        I am using bootstrap
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
